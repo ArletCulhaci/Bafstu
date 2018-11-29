83211ce37d70481770d4caee329ba411b8786d81
ArletCulhaci <s1082434@student.hsleiden.nl>
2018-11-29 11:26:35 +0100

    plot function added

--- prep_IBEDs_pipeline.sh
+++ prep_IBEDs_pipeline.sh
@@ -51,10 +51,21 @@ if [ "$#" -eq 10 ]; then
     done
 
     mkdir "${3}/Clean_$DATE-$N"	
-    process_radtags -f "${3}"/"${1}" -o ${3}/"Clean_$DATE-$N" -b "${3}"/"${2}" -e ${4} -r -t ${5} -q -D  -s ${6} -w ${7}
+    process_radtags -f "${3}"/"${1}" -o ${3}/"Clean_$DATE-$N" -b "${3}"/"${2}" -e ${4} -r -t ${5} -q -D  -s ${6} -w ${7} &>/dev/null &
+    PID=$!
+    i=1
+    sp="/-\|"
+    echo -n ' '
+    while [ -d /proc/$PID ]
+        do	
+	    echo "Process_radtags is running"
+            printf "\b${sp:i++%${#sp}:1}"
+#\b${sp:i++%${#sp}:1}"
+        done
     echo  process_radtags -f "${3}"/"${1}" -o ${3}/"Clean_$DATE-$N" -b "${3}"/"${2}" -e ${4} -r -t ${5} -q -D  -s ${6} -w ${7}  > ${3}/"Clean_$DATE-$N"/Command_log.txt
     cat ${3}/process_radtags* | tail -n +12 | awk '{print $2, $3, $6}' | head -${num_ind} | tr ' ' ';' > ${3}/"Clean_$DATE-$N"/Values_run_total_reads_bf_process.txt
-
+    Rscript test_r.R Values_run_total_reads_bf_process.txt ${3}/"Clean_$DATE-$N" --save --quiet 2>&1 >/dev/null
+    echo "A plot containing the initial number of reads before process_radtags can be found in the following directoty ${3}/"Clean_$DATE-$N""
 fi
 
 sleep 30
--- test_r.R
+++ test_r.R
@@ -1,2 +1,24 @@
 args <- commandArgs()
-print(args)
+file <- args[6]
+dir <- args[7]
+library(ggplot2)
+#First install cowplot!
+#library(cowplot)
+file_path <- paste(dir, "/", file, sep="")
+output_path <- paste(dir, "/", "Plot_raw_reads.pdf", sep="")
+#print(output_path)
+data_raw <- read.delim(file_path, sep=";", header = TRUE)
+#data_raw <- data_raw[-c(0,1), ]
+#data_raw$V1 <- as.numeric(as.character(data_raw$V1))
+#data_raw$V2 <- as.numeric(as.character(data_raw$V2))
+#data_raw$V3 <- as.numeric(as.character(data_raw$V3))
+
+a <- ggplot(data_raw, aes(x=data_raw$Filename,y=data_raw$Total, fill=data_raw$Total)) + geom_bar(stat="identity") + 
+  labs(x="Individuals", y="# initial reads") +
+  ggtitle("# reads per sample") +
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size=10), , axis.text=element_text(size=12))
+a+scale_fill_gradient(low="lightblue", high="darkblue")
+pdf(output_path, width=15, height=15)
+plot(a)
+dev.off()
