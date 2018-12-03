2d1399a2c6c6dd525f1a7ebfd308154d930ca49f
ArletCulhaci <36300291+ArletCulhaci@users.noreply.github.com>
2018-12-03 13:46:54 +0100

    Add files via upload

--- sstacks_31_okt.py
+++ sstacks_31_okt.py
@@ -2,18 +2,18 @@
 name: Arlet Culhaci
 Date: 01-nov-2018
 Function: invoke sstacks module
-Version 1:
+Version 1: steffi
 """
 import csv
 import os
 samples = []
-f  = open("../barcodes.csv", "rb")
+f  = open("../../barcodes.csv", "rb")
 reader = csv.reader(f)
 for row in reader:
     samples.append(row[0][6:])
 
 
 for x in range(len(samples)):
-    command1 = "sstacks -c ./stacks_trial7_h_n1_M2.2/ -s ./stacks_trial7_h_n1_M2.2/" + str(samples[x]) + ".forwards  -o ./sstacks_1_nov/ -p 5 2> sstack_1_nov_" + str(samples[x]) + ".log" 
+    command1 = "sstacks -c ./stacks_steffi_ac_1_nov/ -s ./stacks_steffi_ac_1_nov/" + str(samples[x]) + " -o ./stacks_steffi_ac_1_nov/ -p 5 2> ./stacks_steffi_ac_1_nov/sstack_1_nov_" + str(samples[x]) + "_steffi.log" 
     os.system(command1)
 
