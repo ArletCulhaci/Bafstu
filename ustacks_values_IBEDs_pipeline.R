b8f8e1ffcb4f7495b9d707e56f31b9264ba318cf
mtusti05 <36300291+Mtusti05@users.noreply.github.com>
2019-04-28 10:32:08 +0200

    Update ustacks_values_IBEDs_pipeline.R

--- ustacks_values_IBEDs_pipeline.R
+++ ustacks_values_IBEDs_pipeline.R
@@ -2,11 +2,11 @@ args <- commandArgs()
 file <- args[6]
 dir <- args[7]
 library(ggplot2)
-library(cowplot)
-#First install cowplot!
-#library(cowplot)
+
 file_path <- paste(dir, "/", file, sep="")
-output_path <- paste(dir, "/", "Plot_ustacks.pdf", sep="")
+output_path1 <- paste(dir, "/", "Plot_ustacks_reads.pdf", sep="")
+output_path2 <- paste(dir, "/", "Plot_ustacks_stacks.pdf", sep="")
+output_path3 <- paste(dir, "/", "Plot_ustacks_depth.pdf", sep="")
 #print(output_path)
 data_raw <- read.delim(file_path, sep=";", header = FALSE)
 data_raw <- data_raw[-c(0,1), ]
@@ -37,6 +37,14 @@ c <- ggplot(data_raw, aes(x=data_raw$V1,y=data_raw$V10, fill=data_raw$V10)) + ge
   theme(axis.text.x = element_text(angle = 90, hjust = 1, size=10), , axis.text=element_text(size=12))
 c+scale_fill_gradient(low="lightblue", high="darkblue")
 
-pdf(output_path, width=15, height=15)
-plot_grid( a, b, c, labels = c("Reads", "Stacks", "Depth"),label_size = 20)
+pdf(output_path1, width=15, height=15)
+plot(a, main="Reads")
+dev.off()
+
+pdf(output_path2, width=15, height=15)
+plot(b, main="Stacks")
+dev.off()
+
+pdf(output_path3, width=15, height=15)
+plot(c, main="Depth")
 dev.off()
