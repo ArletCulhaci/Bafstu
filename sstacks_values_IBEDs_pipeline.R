804528cad6ee4b3f2ed793fb54efdf2b4b7ced3c
mtusti05 <36300291+Mtusti05@users.noreply.github.com>
2019-04-28 10:30:30 +0200

    Update sstacks_values_IBEDs_pipeline.R

--- sstacks_values_IBEDs_pipeline.R
+++ sstacks_values_IBEDs_pipeline.R
@@ -2,12 +2,11 @@ args <- commandArgs()
 file <- args[6]
 dir <- args[7]
 library(ggplot2)
-library(cowplot)
-#First install cowplot!
-#library(cowplot)
+
 file_path <- paste(dir, "/", file, sep="")
-output_path <- paste(dir, "/", "Plot_sstacks.pdf", sep="")
-#print(output_path)
+output_path1 <- paste(dir, "/", "Plot_sstacks_matching_loci.pdf", sep="")
+output_path2 <- paste(dir, "/", "Plot_sstacks_varified_haplotypes.pdf", sep="")
+
 data_raw <- read.delim(file_path, sep=";", header = FALSE)
 data_raw <- data_raw[-c(0,1), ]
 data_raw$V1 <- as.character(data_raw$V1)
@@ -33,7 +32,9 @@ a3 <- ggplot(data_raw, aes(x=data_raw$V1, y=data_raw$V20, fill=data_raw$V20)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, size=10))
 a3 <- a3 + scale_fill_gradient(low='lightblue',high='darkblue')
 
-pdf(output_path, width=15, height=15)
-plot_grid( a, a3, labels = c("Matching loci", "Verified haplotypes" ),label_size = 20)
+pdf(output_path1, width=15, height=15)
+plot(a, main="Matching loci")
 dev.off()
-
+pdf(output_path2, width=15, height=15)
+plot(a3, main="Varified haplotypes")
+dev.off()
