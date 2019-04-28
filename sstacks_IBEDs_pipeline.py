79795bf0b27abcc0cff07a865adada0861ae5ab4
mtusti05 <36300291+Mtusti05@users.noreply.github.com>
2019-04-28 10:28:15 +0200

    Update sstacks_IBEDs_pipeline.py

--- sstacks_IBEDs_pipeline.py
+++ sstacks_IBEDs_pipeline.py
@@ -11,7 +11,7 @@ def getSampleID(gsi_barcode, gsi_path):
     ls_sampleIDs = []
     print(gsi_path)
     print(gsi_barcode)
-    f  = open(gsi_barcode, "rb")
+    f  = open(gsi_barcode, "r")
     reader = csv.reader(f)
     for row in reader:
         ls_sampleIDs.append(row[0][6:]) #gets remaining string after 6 indices, barcode(5 bp). 
@@ -21,12 +21,10 @@ def executeCommand(ec_sampleIDs, ec_ab_path, ec_output_dir, ec_data_type):
     for x in range(len(ec_sampleIDs)):
         if ec_data_type == "true":
             command1 = "sstacks -c " + ec_ab_path + "/" + ec_output_dir + " -s " + ec_ab_path + "/" + ec_output_dir + "/" + str(ec_sampleIDs[x]) + ".1 -o " + ec_ab_path + "/" + ec_output_dir +" -p 20 2> " + ec_ab_path + "/" + ec_output_dir + "/" + "sstack_" + str(ec_sampleIDs[x]) + ".log"
-            #print(command1)
             os.system(command1)
         else:
-             command1 = "sstacks -c " + ec_ab_path + "/" + ec_output_dir + " -s " + ec_ab_path + "/" + ec_output_dir + "/" + str(ec_sampleIDs[x]) + " -o " + ec_ab_path + "/" + ec_output_dir +" -p 20 2> " + ec_ab_path + "/" + ec_output_dir + "/" + "sstack_" + str(ec_sampleIDs[x]) + ".log"
-             #print(command1)
-             os.system(command1)
+            command1 = "sstacks -c " + ec_ab_path + "/" + ec_output_dir + " -s " + ec_ab_path + "/" + ec_output_dir + "/" + str(ec_sampleIDs[x]) + " -o " + ec_ab_path + "/" + ec_output_dir +" -p 20 2> " + ec_ab_path + "/" + ec_output_dir + "/" + "sstack_" + str(ec_sampleIDs[x]) + ".log"
+            os.system(command1)
 
 
 def main():
