d0282a2d79cc2367cac96cffdeaacbc6b3ab67c2
mtusti05 <36300291+Mtusti05@users.noreply.github.com>
2019-04-28 12:30:34 +0200

    Update convert_vcf_matrix.R

--- convert_vcf_matrix.R
+++ convert_vcf_matrix.R
@@ -3,13 +3,12 @@ args <- commandArgs()
 file <- args[6]
 dir <- args[7]
 library(VariantAnnotation)
-library(snpStats)
 library(GenomeInfoDb)
 file_path <- paste(dir, "/", file, sep="")
 output_path <- paste(dir, "/", "SNP_matrix.csv", sep="")
 #print(output_path)
 
-vcf <- readVcf(file_path)
+vcf <- readVcf(file_path, genome="")
 
 matr <- genotypeToSnpMatrix(vcf, uncertain=FALSE)
 
