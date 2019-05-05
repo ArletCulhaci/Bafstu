c8b3173f4059e0f724801a71de4bab79807997c0
mtusti05 <36300291+Mtusti05@users.noreply.github.com>
2019-05-05 14:42:00 +0200

    Update ustacks_values_IBEDs_pipeline.R

--- ustacks_values_IBEDs_pipeline.R
+++ ustacks_values_IBEDs_pipeline.R
@@ -20,7 +20,7 @@ a <- ggplot(data_raw, aes(x=data_raw$V1,y=data_raw$V2, fill=data_raw$V2)) + geom
   labs(x="Individuals", y="# initial reads") +
   ggtitle("# reads per sample") +
   theme_bw()+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, size=10), , axis.text=element_text(size=12))
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size=10),  axis.text=element_text(size=12))
 a+scale_fill_gradient(low="lightblue", high="darkblue")
 
 b <- ggplot(data_raw, aes(x=data_raw$V1,y=data_raw$V9, fill=data_raw$V9)) + geom_bar(stat="identity") +
